d1fe5cdb5713e4299e745f845796edaa06ae3ee6
Ravi0728 <ravidubey.iksula@gmail.com>
2022-06-27 10:57:20 +0530

    iNotebook created

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React,{ useContext, useEffect } from "react"
 import noteContext from '../context/notes/noteContext'
 
 const About = () => {
-
+ 
   return (
     <div>
       This is About page.
